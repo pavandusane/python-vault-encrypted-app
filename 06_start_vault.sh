866a6c75bc80a9d3130b605b9ce28a081fc10667
Pavan <abc.gmail.com>
2024-09-19 20:03:21 +0530

    add files

---
+++ 06_start_vault.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#stop and remove vault containers if already running
+docker stop vault-demo-vault
+docker rm vault-demo-vault
+#start Vault in dev mode on port 8200
+#docker run --name vault-demo-vault --network dev-network -p 8200:8200 hashicorp/vault-enterprise:1.4.0_ent server -dev -dev-root-token-id="root" &
+
+docker run --name vault-demo-vault --network dev-network -p 8200:8200 hashicorp/vault:latest server -dev -dev-root-token-id="root" &
